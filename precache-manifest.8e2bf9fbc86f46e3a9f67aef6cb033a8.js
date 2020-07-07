c1ed15bafb9169605a57baed6d16c2e0a136881e
Minsung Kim <dolikart@naver.com>
2020-07-07 11:40:01 +0900

    Updates

---
+++ precache-manifest.8e2bf9fbc86f46e3a9f67aef6cb033a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b4210fb337b36d8349c4dd7bea004b",
+    "url": "/movie-list-project/index.html"
+  },
+  {
+    "revision": "14a436eddbe7296a1847",
+    "url": "/movie-list-project/static/css/main.211beb27.chunk.css"
+  },
+  {
+    "revision": "b8b1dc8b7fbb621926fa",
+    "url": "/movie-list-project/static/js/2.07591bed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-list-project/static/js/2.07591bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a436eddbe7296a1847",
+    "url": "/movie-list-project/static/js/main.21d15a6c.chunk.js"
+  },
+  {
+    "revision": "a60a90f9b26af491d6a8",
+    "url": "/movie-list-project/static/js/runtime-main.9a2ffe29.js"
+  }
+]);
