1141c820102e893a6060ad2ac8cd881c8def35ef
Minsung Kim <dolikart@naver.com>
2021-07-28 21:31:40 +0900

    usePromise 적용 상태

--- React_project/news-viewer/src/components/NewsList.js
+++ React_project/news-viewer/src/components/NewsList.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
 	box-sizing: border-box;
@@ -24,36 +25,25 @@ const NewsListBlock = styled.div`
 // };
 
 const NewsList = ({ category }) => {
-	const [articles, setArticles] = useState(null);
-	const [loading, setLoading] = useState(false);
-
-	useEffect(() => {
-		//async를 사용하는 함수 따로 선언.
-		const fetchData = async () => {
-			setLoading(true);
-			try {
-				const query = category === 'all' ? '' : `&category=${category}`;
-				const response = await axios.get(
-					`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=b296d7212b79483d8d9813d53fa04244`
-				);
-				setArticles(response.data.articles);
-				console.log(response.data);
-			} catch (e) {
-				console.log(e);
-			}
-			setLoading(false);
-		};
-		fetchData();
+	const [loading, response, error] = usePromise(() => {
+		const query = category === 'all' ? '' : `&category=${category}`;
+		return axios.get(
+			`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=b296d7212b79483d8d9813d53fa04244`
+		);
 	}, [category]);
 	//대기중일때
 	if (loading) {
 		return <NewsListBlock>대기중...</NewsListBlock>;
 	}
 	//아직 article 값이 설정되지 않았을 때
-	if (!articles) {
+	if (!response) {
 		return null;
 	}
+	if (error) {
+		return <NewsListBlock>에러 발생!</NewsListBlock>;
+	}
 	//article값이 유효할때
+	const { articles } = response.data;
 	return (
 		<NewsListBlock>
 			{articles.map((article) => (
