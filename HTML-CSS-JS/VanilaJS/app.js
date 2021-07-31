bbcd575bc747ef7cb70e66f6b0decccf2a3b30f1
Minsung Kim <dolikart@naver.com>
2021-07-31 22:02:32 +0900

    기존의 class명 보존하는 법

--- HTML-CSS-JS/VanilaJS/app.js
+++ HTML-CSS-JS/VanilaJS/app.js
@@ -2,10 +2,16 @@ const title = document.querySelector('h1');
 
 function handleTitleClick() {
 	const clickedClass = 'clicked';
-	if (title.className === clickedClass) {
-		title.className = '';
+	// //title.className === clickedClass
+	// if (title.className === clickedClass) {
+	// 	title.className = '';
+	// } else {
+	// 	title.className = clickedClass;
+	// }
+	if (title.classList.contains(clickedClass)) {
+		title.classList.remove(clickedClass);
 	} else {
-		title.className = clickedClass;
+		title.classList.add(clickedClass);
 	}
 }
 title.addEventListener('click', handleTitleClick);
