609aeb48900c48cc8d7dc974465feddae88f69c0
Minsung Kim <dolikart@naver.com>
2021-07-31 22:07:44 +0900

    toggle 사용법

--- HTML-CSS-JS/VanilaJS/app.js
+++ HTML-CSS-JS/VanilaJS/app.js
@@ -8,11 +8,15 @@ function handleTitleClick() {
 	// } else {
 	// 	title.className = clickedClass;
 	// }
-	if (title.classList.contains(clickedClass)) {
-		title.classList.remove(clickedClass);
-	} else {
-		title.classList.add(clickedClass);
-	}
+
+	// if (title.classList.contains(clickedClass)) {
+	// 	title.classList.remove(clickedClass);
+	// } else {
+	// 	title.classList.add(clickedClass);
+	// }
+
+	//위의 5줄을 한줄로 줄일 수 있다. clicked가 class에 있는지 판단하고 없다면 class에 clicked를 추가한다.
+	title.classList.toggle('clicked');
 }
 title.addEventListener('click', handleTitleClick);
 // //event 함수 작성
